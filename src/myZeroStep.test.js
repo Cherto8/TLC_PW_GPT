9b86eafe63cecf2b2354b65f71d88c49d518e472
mutvas <vasil.mutafchiev@tdameritrade.com>
2024-08-16 11:54:16 +0300

    return to chromium

--- src/myZeroStep.test.js
+++ src/myZeroStep.test.js
@@ -3,28 +3,29 @@ import { ai } from '@zerostep/playwright';
 import assert from 'node:assert';
 import { launchChrome, waitFor } from './utils.js';
 
-test('Landing page - Search test', async ({ page }) => {
-  const aiArgs = { page, test }
-  const expectedSearchText = 'Order Statuses'
-
-  await page.goto('https://toslc.thinkorswim.com')
-  await ai('Click on the search box', aiArgs)
-  await ai(`Type "${expectedSearchText}" in the search box`, aiArgs)
-  await ai('Submit', aiArgs)
-  const actualSearchText = await ai('Get summary of the SearchListItem__title', aiArgs)
-  console.log('Highlighted text: ', expectedSearchText)
-  assert.equal(expectedSearchText, actualSearchText)
-})
+//test('Landing page - Search test', async ({ page }) => {
+//  const aiArgs = { page, test }
+//  const expectedSearchText = 'Order Statuses'
+//
+//  await page.goto('https://toslc.thinkorswim.com')
+//  await ai('Click on the search box', aiArgs)
+//  await ai(`Type "${expectedSearchText}" in the search box`, aiArgs)
+//  await ai('Submit', aiArgs)
+//  const actualSearchText = await ai('Get summary of the SearchListItem__title', aiArgs)
+//  console.log('Highlighted text: ', expectedSearchText)
+//  assert.equal(expectedSearchText, actualSearchText)
+//})
 
 test('Article - Video play test', async ({ page }) => {
    // Launch Chrome instead of the default Chromium
-   const browser = await launchChrome();
-   const context = await browser.newContext();
-   const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
-   // An object with page and test must be passed into every call
-   const aiArgs = { page: newPage, test };
+   //const browser = await launchChrome();
+   //const context = await browser.newContext();
+   //const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
+   //// An object with page and test must be passed into every call
 
-   await newPage.goto('https://toslc.thinkorswim.com');
+   //const aiArgs = { page: newPage, test };
+   const aiArgs = { page, test }
+   await page.goto('https://toslc.thinkorswim.com');
    await ai('Click on header link "thinkManual"', aiArgs);
    await ai("Scroll down", aiArgs);
    await ai("Click on the Overlay__playButton__icon button", aiArgs);
@@ -33,5 +34,5 @@ test('Article - Video play test', async ({ page }) => {
    assert.notEqual("00:00/00:00", time);
    console.log("Time: ", time)
    // Close the browser
-   await browser.close();
+   //await browser.close();
 })
