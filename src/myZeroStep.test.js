91daf59d2e4a25fd0a650490e1ca375fa2e56568
mutvas <vasil.mutafchiev@tdameritrade.com>
2024-08-16 13:30:31 +0300

    Chrome browser setup - headless

--- src/myZeroStep.test.js
+++ src/myZeroStep.test.js
@@ -3,6 +3,27 @@ import { ai } from '@zerostep/playwright';
 import assert from 'node:assert';
 import { launchChrome, waitFor } from './utils.js';
 
+test('Article - Video play test', async ({ page }) => {
+   // Launch Chrome instead of the default Chromium
+   const browser = await chromium.launch({channel: 'chrome', headless: true});
+   const context = await browser.newContext();
+   const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
+   // An object with page and test must be passed into every call
+   const aiArgs = { page: newPage, test };
+
+   await newPage.goto('https://toslc.thinkorswim.com');
+   await ai('Click on header link "thinkManual"', aiArgs);
+   await ai("Scroll down", aiArgs);
+   await ai("Click on the Overlay__playButton__icon button", aiArgs);
+   await waitFor(5); //Wait for <seconds>
+   const time = await ai("Get the text of the VideoPlayerControls__controls__time", aiArgs);
+   assert.notEqual("00:00/00:00", time);
+   console.log("Time: ", time)
+   // Close the browser
+   await browser.close();
+})
+
+
 //test('Landing page - Search test', async ({ page }) => {
 //  const aiArgs = { page, test }
 //  const expectedSearchText = 'Order Statuses'
@@ -37,22 +58,4 @@ import { launchChrome, waitFor } from './utils.js';
 //})
 
 
-test('Article - Video play test', async ({ page }) => {
-   // Launch Chrome instead of the default Chromium
-   const browser = await chromium.launch({channel: 'chrome'});
-   const context = await browser.newContext();
-   const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
-   // An object with page and test must be passed into every call
-   const aiArgs = { page: newPage, test };
 
-   await newPage.goto('https://toslc.thinkorswim.com');
-   await ai('Click on header link "thinkManual"', aiArgs);
-   await ai("Scroll down", aiArgs);
-   await ai("Click on the Overlay__playButton__icon button", aiArgs);
-   await waitFor(5); //Wait for <seconds>
-   const time = await ai("Get the text of the VideoPlayerControls__controls__time", aiArgs);
-   assert.notEqual("00:00/00:00", time);
-   console.log("Time: ", time)
-   // Close the browser
-   await browser.close();
-})
