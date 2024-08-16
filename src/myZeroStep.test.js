f55c9747f17e62f71fa64c89003174aacfb1b04e
mutvas <vasil.mutafchiev@tdameritrade.com>
2024-08-16 13:23:09 +0300

    Chrome browser setup

--- src/myZeroStep.test.js
+++ src/myZeroStep.test.js
@@ -3,36 +3,56 @@ import { ai } from '@zerostep/playwright';
 import assert from 'node:assert';
 import { launchChrome, waitFor } from './utils.js';
 
-test('Landing page - Search test', async ({ page }) => {
-  const aiArgs = { page, test }
-  const expectedSearchText = 'Order Statuses'
-
-  await page.goto('https://toslc.thinkorswim.com')
-  await ai('Click on the search box', aiArgs)
-  await ai(`Type "${expectedSearchText}" in the search box`, aiArgs)
-  await ai('Submit', aiArgs)
-  const actualSearchText = await ai('Get search result', aiArgs)
-  console.log('Actual Search Text: ', actualSearchText)
-  assert(actualSearchText.includes(expectedSearchText))
-})
+//test('Landing page - Search test', async ({ page }) => {
+//  const aiArgs = { page, test }
+//  const expectedSearchText = 'Order Statuses'
+//
+//  await page.goto('https://toslc.thinkorswim.com')
+//  await ai('Click on the search box', aiArgs)
+//  await ai(`Type "${expectedSearchText}" in the search box`, aiArgs)
+//  await ai('Submit', aiArgs)
+//  const actualSearchText = await ai('Get search result', aiArgs)
+//  console.log('Actual Search Text: ', actualSearchText)
+//  assert(actualSearchText.includes(expectedSearchText))
+//})
 
 //test('Article - Video play test', async ({ page }) => {
 //   // Launch Chrome instead of the default Chromium
-//   //const browser = await launchChrome();
-//   //const context = await browser.newContext();
-//   //const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
-//   //// An object with page and test must be passed into every call
+//   const browser = await launchChrome();
+//   const context = await browser.newContext();
+//   const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
+//   // An object with page and test must be passed into every call
+//   const aiArgs = { page: newPage, test };
 //
-//   //const aiArgs = { page: newPage, test };
-//   const aiArgs = { page, test }
-//   await page.goto('https://toslc.thinkorswim.com');
+//   await newPage.goto('https://toslc.thinkorswim.com');
 //   await ai('Click on header link "thinkManual"', aiArgs);
 //   await ai("Scroll down", aiArgs);
 //   await ai("Click on the Overlay__playButton__icon button", aiArgs);
-//   await waitFor(10); //Wait for <seconds>
+//   await waitFor(5); //Wait for <seconds>
 //   const time = await ai("Get the text of the VideoPlayerControls__controls__time", aiArgs);
 //   assert.notEqual("00:00/00:00", time);
 //   console.log("Time: ", time)
 //   // Close the browser
-//   //await browser.close();
+//   await browser.close();
 //})
+
+
+test('Article - Video play test', async ({ page }) => {
+   // Launch Chrome instead of the default Chromium
+   const browser = await chromium.launch({channel: 'chrome'});
+   const context = await browser.newContext();
+   const newPage = await context.newPage(); // Use `newPage` instead of `page` to avoid conflict
+   // An object with page and test must be passed into every call
+   const aiArgs = { page: newPage, test };
+
+   await newPage.goto('https://toslc.thinkorswim.com');
+   await ai('Click on header link "thinkManual"', aiArgs);
+   await ai("Scroll down", aiArgs);
+   await ai("Click on the Overlay__playButton__icon button", aiArgs);
+   await waitFor(5); //Wait for <seconds>
+   const time = await ai("Get the text of the VideoPlayerControls__controls__time", aiArgs);
+   assert.notEqual("00:00/00:00", time);
+   console.log("Time: ", time)
+   // Close the browser
+   await browser.close();
+})
